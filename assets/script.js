4187d12041b597140d71952ec03fc520ca2fb514
gbpursuit <glcoronel@up.edu.ph>
2025-05-02 10:49:07 +0800

    portfolio initial

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+// Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 // https://www.youtube.com/watch?v=V-CBdlfCPic
 navbar = document.querySelector(".navbar");
 scrollWatcher = document.querySelector("#data-scroll-watcher");
@@ -12,12 +16,13 @@ function generateDeptModals(depts) {
     let logoContainer = document.getElementById("dept-logos");
     let modalContainer = document.getElementById("dept-modals");
 
-    for (dept of depts) {
+    for (let dept of depts) {
         // Create logo
         let logo = document.createElement("img");
         logo.setAttribute("src", "assets/images/dept_logos/" + dept.id + ".png");
         logo.setAttribute("data-bs-toggle", "modal");
         logo.setAttribute("data-bs-target", "#" + dept.id);
+        logo.setAttribute("tabIndex", "0");
 
         logoContainer.appendChild(logo);
 
@@ -26,12 +31,14 @@ function generateDeptModals(depts) {
         modalDiv.setAttribute("id", dept.id);
         modalDiv.setAttribute("class", "modal fade");
         modalDiv.setAttribute("role", "dialog");
+        modalDiv.setAttribute("tabIndex", "-1");
 
         let modalDialog = document.createElement("div");
         modalDialog.setAttribute("class", "modal-dialog modal-dialog-centered");
 
         let modalContent = document.createElement("div");
         modalContent.setAttribute("class", "modal-content");
+        modalContent.classList.add('customStuff');
         
         let modalHeader = document.createElement("div");
         modalHeader.setAttribute("class", "modal-header");
@@ -50,8 +57,8 @@ function generateDeptModals(depts) {
         heading.innerText = "Data Pre-processing";
         innerCard.appendChild(heading);
         for (desc of dept.description) {
-            let body = document.createElement("p");
-            body.innerHTML = dept.description;
+            var body = document.createElement("p");
+            body.innerHTML = desc;
             innerCard.appendChild(body);
         }
           
@@ -67,6 +74,20 @@ function generateDeptModals(depts) {
 
         modalDiv.appendChild(modalDialog);
         modalContainer.appendChild(modalDiv);
+
+        modalDiv.addEventListener('keydown', function (event) {
+            if (event.key === "Escape") {
+                let modalInstance = new bootstrap.Modal(modalDiv);
+                modalInstance.hide(); 
+            }
+        });
+
+        logo.addEventListener('keydown', function(event) {
+            if (event.key === "Enter") {
+                let modal = new bootstrap.Modal(document.getElementById(dept.id)); 
+                modal.show();
+            }
+        });
     }
 }
 
